cc7c75f122dbf4b2e6111fc800c109c5405601ac
zibasan <164349196+zibasan@users.noreply.github.com>
2026-09-20 18:24:30 +0900

    chore: tailwindcssの導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
+import babel from "@rolldown/plugin-babel"
+import tailwindcss from "@tailwindcss/vite"
+import react, { reactCompilerPreset } from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    tailwindcss(),
+    babel({ presets: [reactCompilerPreset()] }),
   ],
 })
